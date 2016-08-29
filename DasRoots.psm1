1f801cb58c9fc234f8bf340b1b8e24eeb0a6d087
Andrew Lockwood <aclockwood@live.ca>
2016-08-28 23:19:22 -0400

    Added PS module, updated readme

---
+++ DasRoots.psm1
@@ -0,0 +1,26 @@
+﻿<#
+Quick shortcuts for jumping to various commonly used roots
+#>
+function SourceShortcut($DesiredRoot)
+{
+   switch($DesiredRoot)
+   {
+    "go" {cd "C:\Dev\Go"; break;}
+    "c#" {cd "C:\Dev\C#"; break;}
+    "sql" {cd "C:\Dev\SQL"; break;}
+    "java" {cd "C:\Dev\Java"; break;}
+    "duino" {cd "C:\Dev\Arduino"; break;}
+    "js" {cd "C:\Dev\JS"; break;}
+
+    "temp" {cd "C:\temp"; break;}
+    "psm" {cd (Join-Path -Path $env:USERPROFILE -ChildPath "\Documents\WindowsPowerShell\Modules"); break;}
+
+
+    default {cd "C:\Dev\"; break;}
+   }
+}
+Set-Alias root SourceShortcut;
+
+
+#Ensures the aliases are set
+Export-ModuleMember -Alias * -Function *;
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DasRoots
 Ultra-simple PowerShell module I use for jumping to commonly used directories quickly.
+
+## Use
+If you're not me you probably don't use my directory structure. You're probably better off just writing your own version of this and adding it to $profile ;)
